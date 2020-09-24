3ec2440865f9e2f27d5b8c0d49ef7e08fc64a408
VICTOR-TVL <vheecthurikuomola@gmail.com>
2020-09-24 15:20:18 +0100

    Sign in handleSubmit method completed

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 import "./sign-in.styles.scss";
 
@@ -19,10 +19,24 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
 
-    this.setState({ email: "", password: "" });
+    // destructure state
+    const { email, password } = this.state;
+
+    try {
+
+      // sign in with email and password
+      await auth.signInWithEmailAndPassword(email, password);
+
+      // clear state
+      this.setState({ email: "", password: "" });
+
+    } catch (error) {
+      console.log(error);
+    }
+
   };
 
   handleChange = (event) => {
