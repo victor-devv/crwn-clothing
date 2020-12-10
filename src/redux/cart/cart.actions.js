650be0e67126d41d98d70688757573a9ecb6161f
VICTOR-TVL <vheecthurikuomola@gmail.com>
2020-12-10 13:18:48 +0100

    Updated shop data, as well as cart utils to count the quantity of items in cart

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -4,3 +4,8 @@ export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN,
   //payload is an optional property. also we don't need to pass any payload cos the state value is a boolean and the reducer sets the value to the opposite since its a TOGGLE action.
 });
+
+export const addItem = item => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item
+}); //this gets the item that we want to add to the cartItems array and it returns the new action type object which tells the reducer it wants to add an item (payload)
---
+++ src/redux/cart/cart.utills.js
@@ -0,0 +1,18 @@
+// Utility functions allow us to keep our files clean as well as to organize functions that we many need in multiple files in one location
+
+//This func will look into the cartItems array to check if the cartItem we wanna add currently exists in the array
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
