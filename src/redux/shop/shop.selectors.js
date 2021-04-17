5f87bfc8a6f59c530b960bb823f956e75659a2aa
victor-devv <vheecthurikuomola@gmail.com>
2021-04-17 15:37:28 +0100

    Updated favicon and title

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -7,6 +7,13 @@ export const selectCollections = createSelector(
     shop => shop.collections
 )
 
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+
+    // get keys as array, map over them and get value of collection objects as value of each key
+    collections => Object.keys(collections).map(key => collections[key])
+)
+
 export const selectCollection = collectionUrlParam => createSelector(
     [selectCollections],
     collections => collections[collectionUrlParam]
