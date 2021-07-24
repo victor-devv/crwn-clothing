29343fa47b42a6a90edae0ec1127ce78e414aa53
victor-devv <vheecthurikuomola@gmail.com>
2021-07-24 09:22:07 +0100

    Styling changed to styled components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { connect } from 'react-redux'; //this is a higher order component that lets you modify the component to have access to redux
 //higher order components are funcitons that take components as args and reutns a new suped-up component.
 
@@ -15,34 +15,37 @@ import { selectCurrentUSer } from '../../redux/user/user.selectors';
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
-import "./header.styles.scss";
+// import "./header.styles.scss";
+
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink, OptionDiv } from './header.styles'
 
 const Header = ({ currentUser, hidden }) => (
-  <div className="header">
-    <Link className="logo-container" to="/">
+  <HeaderContainer>
+    <LogoContainer to="/">
       <Logo className="logo" />
-    </Link>
-    <div className="options">
-      <Link className="option" to="/shop">
+    </LogoContainer>
+    <OptionsContainer>
+      <OptionLink to="/shop">
         SHOP
-      </Link>
-      <Link className="option" to="/shop">
+      </OptionLink>
+      <OptionLink to="/shop">
         CONTACT
-      </Link>
+      </OptionLink>
       {currentUser ? (
-        <div className="option" onClick={() => auth.signOut()}>
+        <OptionDiv onClick={() => auth.signOut()}>
           SIGN OUT
-        </div>
+        </OptionDiv> 
+        // could also be <OptionLink as='div>, as={ReactComponent}, etc instead of creating two diff consts
       ) : (
-        <Link className="option" to="/signin">
+        <OptionLink to="/signin">
           SIGN IN
-        </Link>
+        </OptionLink>
       )}
 
       <CartIcon />
-    </div>
+    </OptionsContainer>
     {hidden ? null : <CartDropdown />}
-  </div>
+  </HeaderContainer>
 );
 
 //mapStateToProps means put(map) the current app state as a prop to the component
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,12 +2,15 @@ import React from "react";
 
 import Directory from "../../components/directory/directory.component";
 
-import "./homepage.style.scss";
+// import "./homepage.style.scss";
+
+// import css in js
+import { HomePageContainer } from './homepage.styles'
 
 const HomePage = () => (
-  <div className="homepage">
+  <HomePageContainer>
     <Directory />
-  </div>
+  </HomePageContainer>
 );
 
 export default HomePage;
