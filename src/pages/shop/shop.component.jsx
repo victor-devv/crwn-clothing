a526f2e96bc65191e48f9f734511a04bacce6701
victor-devv <vheecthurikuomola@gmail.com>
2021-08-28 15:39:05 +0100

    Imports shop daata from DB to app then adds shop data to Redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,14 +1,46 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import CollectionPage from '../collection/collection.component';
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from "../../firebase/firebase.utils";
 
-const ShopPage = ({ match }) => (
-  <div className='shop-page'>
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-  </div>
-);
+import { updateCollections } from "../../redux/shop/shop.actions";
 
-export default ShopPage;
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import CollectionPage from "../collection/collection.component";
+
+class ShopPage extends React.Component {
+  unsubscribeFromSnapshot = null;
+
+  componentDidMount() {
+    const { updateCollections } = this.props;
+    const collectionRef = firestore.collection('collections');
+
+    //whenever this code gets run for the first time or whenever the collection ref updates
+    collectionRef.onSnapshot(async snapShot => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapShot);
+      updateCollections(collectionsMap)
+    });
+  }
+
+  render() {
+    const { match } = this.props;
+    return(
+      <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  updateCollections: (collectionsMap) =>
+    dispatch(updateCollections(collectionsMap)),
+});
+
+
+export default connect(null, mapDispatchToProps)(ShopPage);
