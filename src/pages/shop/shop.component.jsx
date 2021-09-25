66cebf834c83c940e3d17c97f9c7654a08cc1f4f
victor-devv <vheecthurikuomola@gmail.com>
2021-09-25 13:36:09 +0100

    Highlights different methods of fetching data from a backend source. Observer pattern, promise pattern and native fetch api

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -19,23 +19,46 @@ const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 
 class ShopPage extends React.Component {
-  state ={
+  state = {
     loading: true,
   };
-
 
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
     const { updateCollections } = this.props;
-    const collectionRef = firestore.collection('collections');
+    const collectionRef = firestore.collection("collections");
 
     //whenever this code gets run for the first time or whenever the collection ref updates
-    collectionRef.onSnapshot(async snapShot => {
+
+    //OBSERVER + OBSERVABLE PATTERN / METHOD
+    // this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
+    //   async (snapShot) => {
+    //     const collectionsMap = convertCollectionsSnapshotToMap(snapShot);
+    //     updateCollections(collectionsMap);
+    //     this.setState({ loading: false });
+    //   }
+    // );
+
+    //PROMISE PATTERN / METHOD
+    //the only caveat to using this promise method is that the only time we will get new collections data is when we remount  this component. It doesn't use the live update stram style that the observables pattern provides. 
+    this.unsubscribeFromSnapshot = collectionRef.get().then(snapShot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapShot);
-      updateCollections(collectionsMap)
+      updateCollections(collectionsMap);
       this.setState({ loading: false });
     });
+
+    //FETCH API METHOD
+    //This code is just to show you how it works. The values are too nested here in firebase so we wont waste our time. But this method is useful when we are calling an API of some sort.
+    // fetch(
+    //   "https://firestore.googleapis.com/v1/projects/crwn-db-25327/databases/(default)/documents/collections"
+    // )
+    //   .then(response => response.json())
+    //   .then(collections => console.log(collections));
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromSnapshot();
   }
 
   render() {
